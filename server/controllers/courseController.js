6fe34b4764009ff0f702bed54e57fd96fde11d68
sayak roy <08sayakroy@gmail.com>
2025-08-28 19:55:11 +0530

    "changed the imports"

--- server/controllers/courseController.js
+++ server/controllers/courseController.js
@@ -1,7 +1,5 @@
-import User from '../models/User.js';
 import Course from '../models/Course.js';
-import { Purchase } from '../models/Purchase.js';
-import Stripe from 'stripe';
+
 
 
 //get all courses
