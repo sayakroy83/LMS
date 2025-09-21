5daf10f30c92122af5de5f7f430425107e3ca9f5
sayak roy <08sayakroy@gmail.com>
2025-09-21 19:10:00 +0530

    "updated App.jsx"

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,6 +13,7 @@ import MyCourses from './pages/educator/MyCourses.jsx'
 import StudentsEnrolled from './pages/educator/StudentsEnrolled.jsx'
 import Navbar from './components/student/Navbar.jsx'
 import "quill/dist/quill.snow.css";
+import {ToastContainer} from 'react-toastify'
 
 const App = () => {
 
@@ -20,6 +21,7 @@ const App = () => {
   
   return (
     <div className='text-default min-h-screen bg-white'>
+      <ToastContainer/>
       {!isEducatorRoute && <Navbar/>}
       <Routes>
         <Route path='/' element={<Home/>}/>
