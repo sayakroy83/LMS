b0d33c54a5e3c7a9166572010716de2a1b2f309b
Sayak Roy <08sayakroy@gmail.com>
2025-10-01 13:08:31 +0530

    Update TestimonialsSection.jsx

--- client/src/components/student/TestimonialsSection.jsx
+++ client/src/components/student/TestimonialsSection.jsx
@@ -7,7 +7,7 @@ const TestimonialsSection = () => {
       <h2 className='text-3xl font-medium text-gray-800'>Testimonials</h2>
       <p className='md:text-base text-gray-500 mt-3'>Hear from our leaders as they share their journeys of transformation, success, and how our 
         <br/> platform has made a difference in their lives.</p>
-        <div className='grid grid-cols-3 gap-8 mt-14'>
+        <div className='grid grid-cols-auto gap-8 mt-14'>
           {dummyTestimonial.map((testimonial, index)=>(
             <div key={index} className='text-sm text-left border border-gray-500/30 pb-6 rounded-lg bg-white shadow-[0px_4px_15px_0px] shadow-black/5 overflow-hidden'>
               <div className='flex items-center gap-4 px-5 py-4 bg-gray-500/10'>
@@ -18,7 +18,7 @@ const TestimonialsSection = () => {
                 </div>
               </div>
               <div className='p-5 pb-7'>
-                  <div className='felx gap-0.5'>
+                  <div className='flex gap-0.5'>
                     {[...Array(5)].map((_, i)=> (
                       <img className='h-5' key={i} src={i <Math.floor(testimonial.rating) ? assets.star : assets.star_blank} alt="star" />
                     ))}
